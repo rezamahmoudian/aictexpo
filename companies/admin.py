99d2f8b28087a4b421e40be310abb73a3f418827
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-09 16:34:04 +0330

    add product and product category model

--- companies/admin.py
+++ companies/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
-from.models import Company
+from.models import Company, CompanyCategory
 
 
 # Register your models here.
+class AdminCompany(admin.ModelAdmin):
+    list_display = ('name', 'manager', 'website', 'email', 'mobile')
+    # list_filter = ('publish', 'status', 'author')
+    # ordering = ('-status', '-publish')
+    search_fields = ('name', 'about', 'manager')
+    prepopulated_fields = {'slug': ('name', )}
+    # actions = [make_publish, make_draft]
 
-admin.site.register(Company)
+
+class AdminCompanyCategory(admin.ModelAdmin):
+    list_display = ('title', )
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Company, AdminCompany)
+admin.site.register(CompanyCategory, AdminCompanyCategory)
 
--- companies/models.py
+++ companies/models.py
@@ -12,16 +12,21 @@ class CompanyCategory(models.Model):
         verbose_name = 'دسته بندی'
         verbose_name_plural = 'دسته بندی ها'
 
+    def __str__(self):
+        return self.title
+
 
 class Company(models.Model):
-    name = models.CharField(max_length=200, null=True, verbose_name='عنوان شرکت')
+    name = models.CharField(max_length=200, verbose_name='نام شرکت')
+    slug = models.CharField(max_length=100, unique=True, verbose_name='slug')
     about = models.TextField(verbose_name='درباره شرکت', blank=True, null=True)
     manager = models.CharField(max_length=200, null=True, blank=True, verbose_name="مدیرعامل شرکت")
     manager_photo = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="تصویر مدیرعامل")
     slider = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="بنر شرکت")
+    logo = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="لوگو شرکت")
     video = models.FileField(upload_to='media/video', null=True, blank=True )
-    catalog = models.FileField(upload_to='', null=True, blank=True )
-    catalog_cover = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="جلد کاتالوگ")
+    catalog = models.FileField(upload_to='', null=True, blank=True, verbose_name='کاتالوگ شرکت')
+    catalog_cover = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="جلد کاتالوگ شرکت")
     telephone = models.CharField(max_length=200, null=True, blank=True, verbose_name="تلفن شرکت")
     fax = models.CharField(max_length=200, null=True, blank=True, verbose_name="فکس")
     website = models.CharField(max_length=200, null=True, blank=True, verbose_name="وبسایت")
@@ -38,5 +43,34 @@ class Company(models.Model):
         verbose_name_plural = 'شرکت ها'
 
 
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی محصول')
+    slug = models.CharField(max_length=100, unique=True, verbose_name='آدرس دسته بندی محصول')
+    status = models.BooleanField(default=True, verbose_name='نمایش دادن')
+
+    class Meta:
+        verbose_name = 'دسته بندی محصول'
+        verbose_name_plural = 'دسته بندی های محصولات'
+
+    def __str__(self):
+        return self.title
 
 
+class Product(models.Model):
+    title = models.CharField(max_length=200, verbose_name='نام محصول')
+    slug = models.CharField(max_length=100, unique=True, verbose_name='slug')
+    model = models.CharField(max_length=200, verbose_name='مدل محصول')
+    description = models.TextField(verbose_name='توضیحات محصول', blank=True, null=True)
+    short_description = models.TextField(verbose_name='توضیحات کوتاه محصول', blank=True, null=True)
+    gallery = models.FileField()
+    catalog = models.FileField(upload_to='', null=True, blank=True, verbose_name='کاتالوگ محصول')
+    catalog_cover = models.ImageField(upload_to="images", null=True, blank=True, verbose_name="جلد کاتالوگ محصول")
+    video = models.FileField(upload_to='media/video', null=True, blank=True)
+    show_sertificates = models.BooleanField(verbose_name="نمایش گواهی نامه های شرکت")
+    first_feature = models.CharField(max_length=200, verbose_name='ویژگی اول محصول')
+    second_feature = models.CharField(max_length=200, verbose_name='ویژگی دوم محصول')
+    third_feature = models.CharField(max_length=200, verbose_name='ویژگی سوم محصول')
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="نویسنده",
+                               related_name="product_author")
+    company = models.ForeignKey(Company, verbose_name='نام شرکت', on_delete=models.CASCADE, related_name='product_company')
+    category = models.ManyToManyField(ProductCategory, verbose_name='دسته بندی محصول', related_name="product_category")
