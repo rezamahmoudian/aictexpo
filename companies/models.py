30f85c2166e9a7829ffe4421d0682a76e3675926
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-09 11:45:03 +0330

    complate company model

--- companies/models.py
+++ companies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 # Create your models here.
@@ -9,8 +10,16 @@ class Company(models.Model):
     manager_photo = models.ImageField(upload_to="images", verbose_name="تصویر مدیرعامل")
     slider = models.ImageField(upload_to="images", verbose_name="بنر شرکت")
     video = models.FileField(upload_to='media/video', )
-    catalogue = models.FileField(upload_to='', )
-
-
+    catalog = models.FileField(upload_to='', )
+    catalog_cover = models.ImageField(upload_to="images", verbose_name="جلد کاتالوگ")
+    telephone = models.CharField(max_length=200, verbose_name="تلفن شرکت")
+    fax = models.CharField(max_length=200, verbose_name="فکس")
+    website = models.CharField(max_length=200, verbose_name="وبسایت")
+    email = models.EmailField(max_length=200, verbose_name="ایمیل")
+    mobile = models.CharField(max_length=200, verbose_name="موبایل")
+    address = models.TextField(verbose_name="موبایل")
+    certificates = models.FileField()
+    show_baner = models.BooleanField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name='نویسنده')
 
 
