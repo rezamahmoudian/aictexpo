749744f915d7cd4509069b82e23fbe5c6adabe76
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-10 09:45:39 +0330

    add amdmin product and admin product category class

--- companies/admin.py
+++ companies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Company, CompanyCategory
+from.models import Company, CompanyCategory, Product, ProductCategory
 
 
 # Register your models here.
@@ -17,6 +17,20 @@ class AdminCompanyCategory(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
 
 
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('title', 'short_description', 'author', 'company')
+    search_fields = ('title', 'short_description', 'author')
+    prepopulated_fields = {'slug': ('title', )}
+    # actions = [make_publish, make_draft]
+
+
+class AdminProductCategory(admin.ModelAdmin):
+    list_display = ('title', )
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Company, AdminCompany)
 admin.site.register(CompanyCategory, AdminCompanyCategory)
+admin.site.register(Product, AdminProduct)
+admin.site.register(ProductCategory, AdminProductCategory)
 
