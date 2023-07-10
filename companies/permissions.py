40d472dc5112645f4857c989dd656c9a6cc8b3f8
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-10 13:31:22 +0330

    add permission is staff or readonly to company and product veiws

--- companies/permissions.py
+++ companies/permissions.py
@@ -1,16 +1,15 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-# class IsAuthorOrReadOnly(BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.user.is_authenticated and \
-#                 request.user.is_superuser:
-#             return True
-#         elif request.user.is_authenticated and \
-#                 request.user == obj.author:
-#             return True
-#         elif request.method in SAFE_METHODS:
-#             return True
-#         else:
-#             return False
-
+class IsAuthorOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated and \
+                request.user.is_superuser:
+            return True
+        elif request.user.is_authenticated and \
+                request.user == obj.author:
+            return True
+        elif request.method in SAFE_METHODS:
+            return True
+        else:
+            return False
