05ec28292df5101b550ee18f6f28c87893ae62f5
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-15 09:58:45 +0330

    add apps

--- acount/apps.py
+++ acount/apps.py
@@ -4,4 +4,3 @@ from django.apps import AppConfig
 class AcountConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'acount'
-    # label = 'acount'  # <-- this is the important line - change it to anything other than the default, which is the module name ('acount' in this case)
--- companies/apps.py
+++ companies/apps.py
@@ -2,8 +2,5 @@ from django.apps import AppConfig
 
 
 class CompaniesConfig(AppConfig):
-    # default_auto_field = 'django.db.models.BigAutoField'
-    # name = 'full.python.path.to.your.app.companies'
-    # label = 'my.companies'  # <-- this is the important line - change it to anything other than the default, which is the module name ('acount' in this case)
     default_auto_field = 'django.db.models.BigAutoField'
     name = "companies"
