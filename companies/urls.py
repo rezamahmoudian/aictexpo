b9752e7ba20951c06d0594a81cccf1dbd0f5f690
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-07-17 12:59:47 +0330

    add company api view

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import CompanyCreateListView, ProductCreateListView
+from .views import CompanyCreateListView, ProductCreateListView, CompanyView
+from rest_framework.urlpatterns import format_suffix_patterns
 
 api_name = 'api'
 
 
 urlpatterns = [
     path('companies/', CompanyCreateListView.as_view(), name='home'),
-    path('products/', ProductCreateListView.as_view(), name='home'),
+    path('companie/', CompanyView.as_view(), name='home1'),
+    path('products/', ProductCreateListView.as_view(), name='product'),
 ]
 
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- companies/views.py
+++ companies/views.py
@@ -4,8 +4,46 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import Company, Product
 from .serializers import CompanySerializer, ProductSerializer
 from .permissions import BasePermission, IsAuthorOrReadOnly, IsStaffOrReadOnly
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.views import APIView
 
 # Create your views here.
+
+# @api_view(['GET', 'POST'])
+# @permission_classes((permissions.AllowAny,))
+# def companyView(request):
+#     if request.method == 'GET':
+#         companies = Company.objects.all()
+#         serializer = CompanySerializer(companies, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = CompanySerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CompanyView(APIView):
+    permission_classes = [IsStaffOrReadOnly]
+
+    def get(self, request, format=None):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CompanySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CompanyCreateListView(ListCreateAPIView):
